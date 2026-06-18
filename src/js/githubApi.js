a137dd5798b6e2cd517c82b123025d94c99f9a82
anthony chrisley <achrisley8@gmail.com>
2026-06-17 21:45:40 -0300

    fix: corrigindo renderização das informações do usuário na tela

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,9 +1,8 @@
-    const baseUrl = 'https://api.github.com';
+const baseUrl = 'https://api.github.com';
 
 
 export async function fetchGithubUser(userName){
     const response = await fetch(`${baseUrl}/users/${userName}`)
-
             if (!response.ok) {
                 throw new Error('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
             }
--- src/js/profileView.js
+++ src/js/profileView.js
@@ -15,27 +15,29 @@ export function renderProfile(userData, userRepos, container) {
 `).join('') : '<p>Nenhum repositório encontrado.</p>';
 
     container.innerHTML = `<div class="profile-card">
-            <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
-            <div class="profile-info">
-            <h2>${userData.name}</h2>
-            <p>${userData.bio || 'Não possui biografia cadastrada😥.'}</p>
-            <div>
-            <div>
+                             <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
+                           <div class="profile-info">
+                             <h2>${userData.name}</h2>
+                             <p>${userData.bio || 'Não possui biografia cadastrada😥.'}</p>
+                           </div>
+                          </div>
 
-            <div class ="profile-counters">
-            <div class ="followers">
-            <h4>👥 Seguidores</h4>
-            <span>${userData.followers}</span></div>
-            <div class ="following">
-            <h4>👥 Seguindo</h4>
-            <span>${userData.following}</span></div>
-            </div>
-            
-            <div class="profile-repositories">
-    <h2>Repositórios</h2>
-    <div class="repositories">
-        ${repositoriesHTML}
-    </div>
-</div>`
+                          <div class ="profile-counters">
+                            <div class ="followers">
+                              <h4>👥 Seguidores</h4>
+                              <span>${userData.followers}</span>
+                            </div>
+                            <div class ="following">
+                              <h4>👥 Seguindo</h4>
+                              <span>${userData.following}</span>
+                            </div>
+                          </div>
+
+                          <div class="profile-repositories">
+                             <h2>Repositórios</h2>
+                             <div class="repositories">
+                             ${repositoriesHTML}
+                            </div>
+                        </div>`
 
 }
